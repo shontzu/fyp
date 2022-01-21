48395f0d2bbf5c213b8ce9f72e781cfc97e64be9
yuuchin <shontzuforever@gmail.com>
2022-01-21 14:34:12 +0800

    installed dependencies 	> Vuex, BootstrapVue, Firebase

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuex from 'vuex'
+import { BootstrapVue, /* IconsPlugin */ } from 'bootstrap-vue'
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore";
 
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Make the imported packages available throughout the project 
+Vue.use(BootstrapVue)
+Vue.use(Vuex)
+
+// Preventing production start message used for instructions.
 Vue.config.productionTip = false
 
 new Vue({
