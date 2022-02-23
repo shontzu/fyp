4ab1f2bc759c92a7c9ec305962ab1df70a872065
yuuchin <shontzuforever@gmail.com>
2022-02-23 13:58:59 +0800

    init firebase

--- src/main.js
+++ src/main.js
@@ -2,18 +2,29 @@ import Vue from 'vue'
 import App from './App.vue'
 import Vuex from 'vuex'
 import router from './router'
-
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import { BootstrapVue, /* IconsPlugin */ } from 'bootstrap-vue'
 // import { initializeApp } from "firebase/app";
 // import { getFirestore } from "firebase/firestore";
 
-// Import Bootstrap an BootstrapVue CSS files (order is important)
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+const firebaseConfig = {
+  apiKey:process.env.apiKey,
+  authDomain:process.env.authDomain,
+  databaseURL:process.env.databaseURL,
+  projectId: process.env.projectId,
+  storageBucket:process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId
+};
+
 
 // Make the imported packages available throughout the project 
 Vue.use(BootstrapVue)
 Vue.use(Vuex)
+Vue.use(firebaseConfig)
 
 // Preventing production start message used for instructions.
 Vue.config.productionTip = false
