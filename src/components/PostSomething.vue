0de08c3d9b18ce325974059ddbd608aebdc7d675
yuuchin <shontzuforever@gmail.com>
2022-03-30 19:00:11 +0800

    separate data into local json db, fail attempt at FS

--- src/components/PostSomething.vue
+++ src/components/PostSomething.vue
@@ -1,58 +1,89 @@
 <template>
   <div>
-    <span style="display:flex; justify-content:space-between;">
-      <input type="text" v-model="newPost.author" placeholder="author" />
-      <input disabled v-model="newPost.postedOn"/>
-      <button class="btn btn-outline-warning" type="button" @click="addNewPost()">Post</button>
-    </span>
-    <textarea class="md-textarea form-control" rows="3" v-model="newPost.postMsg" placeholder="Post Something"></textarea>
-    <div v-for="post in posts" :key="post.id">
-        <b>{{post.author}}</b> on {{post.postedOn}} <br>
-        {{post.postMsg}}
-        <hr>
+    <div class="card">
+      <span
+        style="
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+        "
+      >
+        <input type="text" v-model="newPost.author" placeholder="author" />
+        <div>{{ newPost.postedOn }}</div>
+        <button
+          class="btn btn-outline-warning"
+          type="button"
+          @click="addNewPost()"
+        >
+          Post
+        </button>
+      </span>
+      <input type="text" v-model="newPost.postTitle" placeholder="Title" />
+      <textarea
+        class="md-textarea form-control"
+        rows="3"
+        v-model="newPost.postMsg"
+        placeholder="What would you like to share?"
+      />
+    </div>
+
+
+    <div class="card" v-for="post in posts" :key="post.id">
+      <h2 class="card-title">{{ post.postTitle }}</h2>
+      <p class="card-text">
+        {{ post.author }} on {{ post.postedOn }} <br />
+        {{ post.postMsg }}
+      </p>
+      <hr />
     </div>
   </div>
 </template>
 
 <script>
+import postsData from "../data/Posts.json"
+// const fs = require('fs');
+
 var date = new Date();
 
 export default {
+  name: "PostSomething",
+  components: {
+  },
   data() {
     return {
-        newPost: {
-            author: null,
-            postedOn: date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear(),
-            postMsg: null,
-        },
-      posts: [
-          {
-              "author": "yuuchin",
-              "postMsg": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe odit velit amet et quo commodi atque iste veniam ipsam quidem?",
-              "postedOn": "03/03/2022"
-          },
-           {
-              "author": "yuuchin",
-              "postMsg": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui numquam necessitatibus cupiditate nostrum vero? Tempora porro tenetur ex dolores quasi veniam, voluptas voluptate iste dolor illum, expedita quidem accusamus explicabo?",
-              "postedOn": "04/03/2022"
-          }
-        ],
+      newPost: {
+        author: "",
+        postTitle: "",
+        postMsg: "",
+        postedOn: date.getDate() +"/" + (date.getMonth() + 1) + "/" + date.getFullYear(),
+      },
+      posts: postsData,
+    // posts: fs.readFileSync('../data/Posts.json'),
+    // object: JSON.parse(this.posts),
     };
   },
   methods: {
-      addNewPost(){
-          if (this.newPost.author=="") {
-              alert("pelase enter your alias");
-          } else if ( this.newPost.postMsg=="") {
-              alert("post cannot be empty");
-          } else {
-                this.posts.push({...this.newPost});
-              console.log(this.posts);
-          }
+    addNewPost() {
+      if (this.newPost.author == "") {
+        alert("please enter your alias");
+      } else if (this.newPost.postTitle == "") {
+        alert("Post title cannot be empty");
+      } else if (this.newPost.postMsg == "") {
+        alert("post cannot be empty");
+      } else {
+        // this.object.push({ ...this.newPost });
+        // console.log(this.object);
+        this.posts.push({ ...this.newPost });
+        console.log(this.posts);
+        this.newPost.author == "";
+        this.newPost.postTitle == "";
+        this.newPost.postMsg == "";
       }
+    },
   },
 };
 </script>
 
-<style>
+<style scoped>
+
 </style>
